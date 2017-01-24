2ab5d6c455f8d5917d09a02b2b083d1e15348278
Lukas Dratwa <lukas.dratwa@stud.tu-darmstadt.de>
2017-01-24 02:47:13 +0100

    Implemented statistic methods for CryptoPass

--- src/main/java/confcost/model/CryptoPass.java
+++ src/main/java/confcost/model/CryptoPass.java
@@ -31,4 +31,225 @@ public class CryptoPass {
 	public String toString() {
 		return this.algorithm+"/"+this.keyExchange+" ("+this.keyLength+","+this.messageLength+") *"+numIterations;
 	}
+	
+	public long getAvgEncryptionTime() {
+		long sum = 0;
+		for(CryptoIteration ci : iterations) {
+			sum += ci.getEncryptionTime();
+		}
+		return sum/iterations.size();
+	}
+	
+	public CryptoIteration getMinEncryptionTime() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getEncryptionTime() < result.getEncryptionTime()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public CryptoIteration getMaxEncryptionTime() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getEncryptionTime() > result.getEncryptionTime()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	
+	public long getAvgDecryptionTime() {
+		long sum = 0;
+		for(CryptoIteration ci : iterations) {
+			sum += ci.getDecryptionTime();
+		}
+		return sum/iterations.size();
+	}
+	
+	public CryptoIteration getMinDecryptionTime() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getDecryptionTime() < result.getDecryptionTime()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public CryptoIteration getMaxDecryptionTime() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getDecryptionTime() > result.getDecryptionTime()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	
+	public long getAvgInitTime() {
+		long sum = 0;
+		for(CryptoIteration ci : iterations) {
+			sum += ci.getInitTime();
+		}
+		return sum/iterations.size();
+	}
+	
+	public CryptoIteration getMinInitTime() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getInitTime() < result.getInitTime()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public CryptoIteration getMaxInitTime() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getInitTime() > result.getInitTime()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	
+	public long getAvgRemoteInitTime() {
+		long sum = 0;
+		for(CryptoIteration ci : iterations) {
+			sum += ci.getRemoteInitTime();
+		}
+		return sum/iterations.size();
+	}
+	
+	public CryptoIteration getMinRemoteInitTime() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getRemoteInitTime() < result.getRemoteInitTime()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public CryptoIteration getMaxRemoteInitTime() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getRemoteInitTime() > result.getRemoteInitTime()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	
+	public long getAvgMessageLength() {
+		long sum = 0;
+		for(CryptoIteration ci : iterations) {
+			sum += ci.getMessageLength();
+		}
+		return sum/iterations.size();
+	}
+	
+	public CryptoIteration getMinMessageLength() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getMessageLength() < result.getMessageLength()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public CryptoIteration getMaxMessageLength() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getMessageLength() > result.getMessageLength()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	
+	public long getAvgKeyLength() {
+		long sum = 0;
+		for(CryptoIteration ci : iterations) {
+			sum += ci.getKeyLength();
+		}
+		return sum/iterations.size();
+	}
+	
+	public CryptoIteration getMinKeyLength() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getKeyLength() < result.getKeyLength()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public CryptoIteration getMaxKeyLength() {
+		CryptoIteration result = null;
+		for(CryptoIteration ci : iterations) {
+			if(result == null) {
+				result = ci;
+			} else {
+				if(ci.getKeyLength() > result.getKeyLength()) {
+					result = ci;
+				}
+			}
+		}
+		return result;
+	}
 }
