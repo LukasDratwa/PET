09448a1d4fbacba32c0c1801d11c7d39bd7eea1a
Marc Eichler <marc@fam-eichler.de>
2017-03-02 22:18:38 +0000

    Reworked to be instantiated, added Javadoc

--- src/main/java/confcost/confcost/App.java
+++ src/main/java/confcost/confcost/App.java
@@ -1,50 +1,66 @@
 package confcost.confcost;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import confcost.controller.Controller;
 import confcost.model.Model;
 import confcost.view.MainFrame;
 
 /**
- * Hello world!
- *
+ * The application main class
  */
 public class App 
 {
-	@SuppressWarnings("unused")
-	private static void setLookAndFeel() {
+	/**
+	 * Sets the L&F to the specified L&F. If no L&F with the specified name exists, the systems' L&F is used instead. 
+	 * @param lf	The L&F name
+	 */
+	public void setLookAndFeel(final String lf) {
 		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (UnsupportedLookAndFeelException | IllegalAccessException | ClassNotFoundException | InstantiationException e) {
-			System.err.println("Unable to set look and feel!");
-			e.printStackTrace();
-	    } 
-	}
-	
-	private static void setNimbusLookAndFeel() {
-		try {
+			boolean found = false;
+			
 		    for(LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
+		        if (info.getName().equals(lf)) {
 		            UIManager.setLookAndFeel(info.getClassName());
+		            found = true;
 		            break;
 		        }
 		    }
-		} catch (Exception e) {
+		    
+		    if (!found)
+		    	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
 		    e.printStackTrace();
 		}
 	}
 	
-    public static void main( String[] args ) throws IOException
-    {
-    	setNimbusLookAndFeel();
+	/**
+	 * Starts the application.
+	 * 
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	public void start() throws UnknownHostException, IOException {
         Model model = new Model();
         MainFrame view = new MainFrame("confcost", model);
         Controller controller = new Controller(model, view);
         controller.start();
+	}
+	
+	/**
+	 * The main function
+	 * @param args	CL args
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+    public static void main( String[] args ) throws UnknownHostException, IOException {
+    	App app = new App();
+    	app.setLookAndFeel("Nimbus");
+    	app.start();
     }
 }
